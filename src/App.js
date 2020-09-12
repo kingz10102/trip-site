0295a48283cfcc511ade09fd06a60a3d43dde499
kingz10102 <kingz10102@gmail.com>
2020-09-11 23:25:33 -0400

    this a commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hi kevin the full-stack developer</h1>
       {/* Home */}
       <Home/>
+      {/* Header */}
+      <Header/>
     </div>
   );
 }
