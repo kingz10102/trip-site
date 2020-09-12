b5d1c6917024a40896292d0f7a43262dff86cfff
kingz10102 <kingz10102@gmail.com>
2020-09-12 16:23:47 -0400

    this a commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import Home from './components/Home';
 import Header from './components/Header';
+import SearchPage from './components/SearchPage';
 import Footer from './components/Footer';
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+     <Router>
       {/* Header */}
         <Header/>
-      {/* Home */}
-      <Home/>
-      {/*  */}
+  
+      <Switch>
+        <Route path="/search">
+         {/* Search Page */}
+        <SearchPage/>
+      </Route>
+      
+      <Route path="/">
+      {/* Home Page */}
+        <Home/>
+     </Route>
+        </Switch>
+      {/* Footer */}
       <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/SearchPage.css';
+function SearchPage() {
+    return (
+        <div className="sarchPage">
+            
+        </div>
+    )
+}
+
+export default SearchPage
