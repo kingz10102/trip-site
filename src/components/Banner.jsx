f11e027052436ba15561378717c7e99978d7fa8d
kingz10102 <kingz10102@gmail.com>
2020-09-12 02:16:08 -0400

    this a commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React,{ useState }from 'react';
 import '../css/Banner.css';
 import { Button } from '@material-ui/core';
 
 function Banner() {
+    // provide functionality with search date button
+    const [showSearch, setShowSearch] = useState(false);
+    
     return (
         <div className="banner">
+            <div className="banner__search">
+                <Button
+                className="banner__searchButton"
+                variant="outlined">
+                    Search Dates
+                </Button>
+            </div>
             <div className="banner__info">
                 <h1>Journey to your Destination</h1>
                 <h5>
