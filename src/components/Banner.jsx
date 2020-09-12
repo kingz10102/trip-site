8c306c3fa51be8b1cf760885047e2058fe8b96f6
kingz10102 <kingz10102@gmail.com>
2020-09-12 01:35:15 -0400

    this a commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import '../css/Banner.css'
+import React from 'react';
+import '../css/Banner.css';
 
 function Banner() {
     return (
         <div className="banner">
-            
+            <div className="banner__info">
+                <h1>Journey to your Destination</h1>
+                <h5>
+                    Lorem ipsum dolor sit amet 
+                    consectetur adipisicing 
+                    elit. Officia, 
+                </h5>
+            </div>
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Banner from '../css/Banner.css'
-import '../css/Home.css'
+import Banner from '../components/Banner';
+import '../css/Home.css';
 
 function Home() {
     return (
         <div className="home">
           {/* Banner */}
-          
+        <h1>hi good people</h1>
           <Banner/>
         </div>
     )
