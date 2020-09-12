0c03707b7cf0f026ca462ff88564e0cf86d15504
kingz10102 <kingz10102@gmail.com>
2020-09-11 23:28:00 -0400

    this a commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import '../css/Header.css;'
+import '../css/Header.css';
 
 function Header() {
     return (
-        <div className="Header">
+        <div className="header">
             <h1>I am the header</h1>
         </div>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import '../css/Home.css'
 
 function Home() {
     return (
-        <div className="Home">
+        <div className="home">
             <h1>I am the home component</h1>
         </div>
     )
