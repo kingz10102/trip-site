b9eeeee274bb2514d7db41b8603064b5a057650e
kingz10102 <kingz10102@gmail.com>
2020-09-12 02:54:11 -0400

    this a commit

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { DateRangePicker } from "react-date-range";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import '../css/DatePicker.css'
-import DatePicker from "react-datepicker";
 
 function DatePicker() {
+    const [startDate, setStartDate] = useState (new Date());
+    const [endDate, setEndDate] =useState(new Date());
+
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection",
+    };
+
+    function handleSelect(ranges) {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+        
+    }
+
+
     return (
         <div className="datePicker">
             
